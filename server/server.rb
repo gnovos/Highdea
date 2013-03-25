d1c82974c953c31d0a9fb22ed1a703e080dc216e
Mason <freemason@gmail.com>
2013-03-25 15:03:22 -0700

    test

--- server/server.rb
+++ server/server.rb
@@ -1,5 +1,25 @@
+require 'rubygems'
 require 'sinatra'
 
-get '/' do
-    "Hello, world"
+configure :production do
+  # Configure stuff here you'll want to
+  # only be run at Heroku at boot
+
+  # TIP:  You can get you database information
+  #       from ENV['DATABASE_URI'] (see /env route below)
+end
+
+
+HTTP_OK = 200
+HEADERS = {
+}
+
+get '/api/:version/:call' do
+  body = ""
+
+  [HTTP_OK, HEADERS, body]
+end
+
+get '/senv' do
+   ENV.inspect
 end
